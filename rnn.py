1f2839fea3f30b90cd382b2e206b7b1416c29720
Chernyshov <xpom55@mail.ru>
2018-05-07 19:22:35 +0300

    Added reshaping for adding new indicators for predicting price !!! !!!!!!! very important!

--- rnn.py
+++ rnn.py
@@ -21,11 +21,24 @@ training_set_scaled = sc.fit_transform(training_set)
 # Creating a data structure with 60 timesteps and 1 output
 X_train = []
 y_train = []
+# подготавливаем данные для нейронки
 for i in range(60, 1258): # с 60 до 1257
   X_train.append(training_set_scaled[i - 60 : i, 0]) # заполняем по 60 элементов
   y_train.append(training_set_scaled[i, 0])
 
 X_train, y_train = np.array(X_train), np.array(y_train)
+# переводим все в правильный формат все данные
+
+# Reshaping
+X_train = np.reshape(
+  X_train,
+  ( # keras documentation => Recurrent Layers => input shapes => 3D tensor (array) with shape
+    X_train.shape[0], # 1198, # количество строчек
+    X_train.shape[1], # 60 # количество столбцов
+    1 # количество индикаторов, 3 измерение в данных, допустим еще акции apple
+  )
+)
+
 
 # Part 2 - Building the RNN
 
